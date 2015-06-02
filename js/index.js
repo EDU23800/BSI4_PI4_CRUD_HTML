cef4be354b33573c1bddf026de79911a73a750a6
EDU23800 <Duuh@MacBook-Pro-de-Eduardo.local>
2015-06-02 18:33:39 -0300

    Chamar telas via javascript

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 var ID_USUARIO = getParameterByName("usuario");
 
-function abrirPerfil(){
+function abrirPerfil(  ){
 
 	window.open( "perfil.html?usuario=" + ID_USUARIO,"_self" );
+
+}
+
+function abrirAbastecimento(){
+
+	window.open( "adicionar.html?usuario=" + ID_USUARIO,"_self" );
 
 }
