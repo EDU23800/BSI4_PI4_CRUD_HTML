88f158e0da61f05df56e5ad5086597c04b5f7e8d
EDU23800 <Duuh@MacBook-Pro-de-Eduardo.local>
2015-06-02 14:50:19 -0300

    Criação de usuário + login

--- js/bemvindo.js
+++
@@ -1,6 +0,0 @@
-function abrirPopPUP(){
-	alert("ops");
-	var usuario = 1;
-	window.open("perfil.html?usuario=" + usuario,"_self");
-
-}
--- js/perfil2.js
+++ js/perfil2.js
@@ -119,10 +119,3 @@ function clica2(){
 	alert(parametro2);
 
 }
-
-function getParameterByName(name) {
-    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
-    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
-}*/
-
-
